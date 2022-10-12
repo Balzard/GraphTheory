b19f80a1a4f1ec4a194d0ac97895f0bd9b5e9b72
Balzard <steph-wormi@hotmail.com>
2022-10-12 11:28:08 +0200

    recursive kosaraju

--- Project1/checker.py
+++ Project1/checker.py
@@ -33,7 +33,7 @@ def read_and_solve_tests(input_file, output_file):
     n_prob = int(f_in.readline())
     for _ in range(n_prob):
         adj = load_graph(f_in)
-        f_out.write(str(solve.solve(adj)) + "\n")
+        f_out.write(str(solve(adj)) + "\n")
 
     f_in.close()
     f_out.close()
@@ -66,17 +66,17 @@ def compare_files(file1, file2):
     Test for one file
 """
 def do_test(test):
-    input_file = "Project1/tests/input"+str(test)+".txt"
-    expected_output_file = "Project1/tests/output"+str(test)+".txt"
+    input_file = "tests/input"+str(test)+".txt"
+    expected_output_file = "tests/output"+str(test)+".txt"
 
-    read_and_solve_tests(input_file, "Project1/tmp/output"+str(test)+".txt")
-    ok,errorLine = compare_files(expected_output_file, "Project1/tmp/output"+str(test)+".txt")
+    read_and_solve_tests(input_file, "tmp/output"+str(test)+".txt")
+    ok,errorLine = compare_files(expected_output_file, "tmp/output"+str(test)+".txt")
     if not ok:
         print("Difference between output and expected output for test : "+str(test)+ " on line : "+str(errorLine))
     else:
         print("Test "+str(test)+" is ok :)")
 
-for i in range(2):
+for i in range(5):
     do_test(i+1)
 
 
--- Project1/solve.py
+++ Project1/solve.py
@@ -1,59 +1,186 @@
 from collections import deque
+from pydoc import visiblename
 
-
-class Graph:
+class Kosaraju:
     
-    def __init__(self, matrix) -> None:
-        self.matrix = matrix
-        self._visited = [False] * len(self.matrix)
-        self._stack = deque()
-        self.nbSCC = 0
-        self.transpose = [[x+1 for x in range(len(matrix)) if y+1 in matrix[x]] for y in range(len(matrix))]
+    def __init__(self, adj) -> None:
+        self.adj = adj
+        self.transpose = [[x for x in range(len(adj)) if y in adj[x]] for y in range(len(adj))]
+        self.N = len(adj)
+        self.visited = [False] * self.N
+        self.stack = deque()
+        self.components = [None] * self.N
         
-    def dfs(self, vertex: int) -> None:
-        self._visited[vertex] = True
-        for i in self.transpose[vertex]:
-            if not self._visited[i]:
-                self.dfs(i)
+    def _visit(self, vertex):
+        if not self.visited[vertex]:
+            self.visited[vertex] = True
+            for neighbor in self.adj[vertex]:
+                self._visit(neighbor)
+            self.stack.append(vertex)
+            
+    def _assign(self, u, r):
+        if self.visited[u]:
+            self.visited[u] = False
+            self.components[u] = r
+            for neighbor in self.transpose[u]:
+                self._assign(neighbor, r)
+                
+    def getComponents(self):
+        for i in range(self.N):
+            self._visit(i)
+            
+        while self.stack:
+            u = self.stack.pop()
+            self._assign(u,u)
+            
+        return self.components
+    
+    
+class IterativeKosaraju(Kosaraju):
+    
+    def __init__(self, adj) -> None:
+        super().__init__(adj)
+    
+    def getComponents(self):
+        for vertex in range(self.N):
+            if not self.visited[vertex]:
+                self.visited[vertex], S = True, [vertex]
+                while S:
+                    vertex, done = S[-1], True
+                    for neighbor in self.adj[vertex]:
+                        if not self.visited[neighbor]:
+                            self.visited[neighbor], done = True, False
+                            S.append(neighbor)
+                            break
+                        if done:
+                            S.pop()
+                            self.stack.append(vertex)
+                            
+        while self.stack:
+            r = self.stack.pop()
+            S = [r]
+            if self.visited[r]:
+                self.visited[r], self.components[r] = False, r
+            while S:
+                u, done = S[-1], True
+                for neighbor in self.transpose[u]:
+                    if self.visited[neighbor]:
+                        self.visited[neighbor] = done = False
+                        S.append(neighbor)
+                        self.components[neighbor] = r
+                        break
+                    
+                if done:
+                    S.pop()
+                    
+        return self.components
+
+
+
+def kosaraju_iterative(G):
+    
+    # postorder DFS on G to transpose the graph and push root vertices to L
+    N = len(G)
+    T, L, U = [[] for _ in range(N)], [], [False] * N
+    for u in range(N):
+        if not U[u]:
+            U[u], S = True, [u]
+            while S:
+                u, done = S[-1], True
+                for v in G[u]:
+                    T[v].append(u)
+                    if not U[v]:
+                        U[v], done = True, False
+                        S.append(v)
+                        break
+                if done:
+                    S.pop()
+                    L.append(u)
+    
+    # postorder DFS on T to pop root vertices from L and mark SCCs
+    C = [None] * N
+    while L:
+        r = L.pop()
+        S = [r]
+        if U[r]:
+            U[r], C[r] = False, r
+        while S:
+            u, done = S[-1], True
+            for v in T[u]:
+                if U[v]:
+                    U[v] = done = False
+                    S.append(v)
+                    C[v] = r
+                    break
+            if done:
+                S.pop()
+    
+    return C
+
+def SCC_to_min_kot(sol,adj):
+    unique= len(set(sol)) # number of scc in graph
+    tmp = [0 for _ in range(max(sol)+1)]  #tmp is used to keep track of the SCC which have an incoming node
+
+    for i in range(len(adj)): #we loop over the edges knowing it starts from node i
+        for x in adj[i]: #This means that there is an edge from i to x
+            if (sol[x] != sol[i]): #If they are not in the same SCC
+                tmp[sol[x]]=1
+    return(unique-sum(tmp)) #(nb min kot) = (nb SCC) - (nbr SCC which have incoming edge)
+      
+def solve(adj):
+    g = IterativeKosaraju(adj)
+    l = g.getComponents()
+    return SCC_to_min_kot(l, adj)  
+
+# adj = [[1], [0, 2], [0, 3, 4], [4], [5], [6], [4], [6]]
+# g = IterativeKosaraju(adj)
+# c = g.getComponents()
+# print(c)
+
+# class Graph:
+    
+#     def __init__(self, matrix) -> None:
+#         self.matrix = matrix
+#         self._visited = [False] * len(self.matrix)
+#         self._stack = deque()
+#         self.nbSCC = 0
+#         self.transpose = [[x for x in range(len(matrix)) if y in matrix[x]] for y in range(len(matrix))]
+        
+#     def dfs(self, vertex: int) -> None:
+#         self._visited[vertex] = True
+#         for i in self.transpose[vertex]:
+#             if not self._visited[i]:
+#                 self.dfs(i)
         
             
-    def fillOrder(self, vertex: int) -> None:
-        self._visited[vertex] = True
-        for i in self.matrix[vertex]:
-            if not self._visited[i]:
-                self.fillOrder(i)
-        self._stack.append(vertex)
+#     def fillOrder(self, vertex: int) -> None:
+#         self._visited[vertex] = True
+#         for i in self.matrix[vertex]:
+#             if not self._visited[i]:
+#                 self.fillOrder(i)
+#         self._stack.append(vertex)
         
     
-    def getNbSCC(self) -> int:
-        #1
-        for i in range(len(self.matrix)):
-            if not self._visited[i]:
-                self.fillOrder(i)
+#     def getNbSCC(self) -> int:
+#         sol = [0 for _ in range(len(self.matrix))]
+#         #1
+#         for i in range(len(self.matrix)):
+#             if not self._visited[i]:
+#                 self.fillOrder(i)
         
-        self._visited = [False] * len(self.matrix)
+#         self._visited = [False] * len(self.matrix)
         
-        #3
-        while self._stack:
-            i = self._stack.pop()
-            if not self._visited[i]:
-                self.dfs(i) # make with transpose
-                self.nbSCC += 1
+#         #3
+#         tmp = 0
+#         while self._stack:
+#             i = self._stack.pop()
+#             if not self._visited[i]:
+#                 self.dfs(i) # make with transpose
+#                 sol[i] = tmp
+#                 self.nbSCC += 1
+#             tmp += 1
                 
-        return self.nbSCC
-    
-
-def solve(adj):
-    graph = Graph(adj)
-    nbSCC = graph.getNbSCC()
-    return nbSCC
-
-matrix = [[2],[3],[4,5],[1],[6],[7],[5,8],[]]
-graph = Graph(matrix)
-transpose = graph.transpose
-print(transpose)
-nbSCC = graph.getNbSCC()
-print(nbSCC)
+#         return sol
 
 
 # """
